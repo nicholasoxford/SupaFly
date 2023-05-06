6bb215f6cdcbb347c10b8183c055a6aa18e4940d
Nicholas Oxford <maheledaily@gmail.com>
2023-05-05 21:11:50 -0600

    meta deploy

--- apps/cli/dist/index.mjs
+++ apps/cli/dist/index.mjs
@@ -8,21 +8,78 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
 import { spawn } from "child_process";
 import figlet from "figlet";
 import { Command } from "commander";
-import { confirm } from "@inquirer/prompts";
+import { select, confirm, input } from "@inquirer/prompts";
 import chalk from "chalk";
+import ora from "ora";
+console.log(figlet.textSync("Supa", "Larry 3D"));
+console.log(figlet.textSync("Based", "Larry 3D"));
 const program = new Command();
-// Async Iteration available since Node 10
-main();
-async function main() {
-    program
-        .version("0.0.1")
-        .description("Deploy Supbase to Fly.io")
-        .option("-o, --org  [value]", "Fly.io Target Organization")
-        .option("-y, --yes", "Skip prompts and deploy")
-        .parse(process.argv);
-    const options = program.opts();
-    console.log(figlet.textSync("Supa", "Larry 3D"));
-    console.log(figlet.textSync("Based", "Larry 3D"));
+program
+    .version("0.0.1")
+    .description("Deploy Supbase to Fly.io")
+    .option("-O, --org  [value]", "Fly.io Target Organization")
+    .option("-y, --yes", "Skip prompts and deploy")
+    .option("-r, --region [value]", "Fly.io Target Region")
+    .option("--dbUrl [value]", "Fly.io Target Region")
+    .parse(process.argv);
+const options = program.opts();
+takeoff();
+async function takeoff() {
+    var _a;
+    // create info object to pass around
+    let info = {
+        username: "",
+        defaultRegion: "",
+        organization: "",
+    };
+    const authSpinner = ora({
+        text: `Checking fly cli authorization...`,
+        color: "yellow",
+    }).start();
+    // grab username
+    info.username = await userAuth(options, authSpinner);
+    authSpinner.stop();
+    console.log("Deploying database as:", chalk.green(info.username));
+    // chose default region if not passed in
+    info.defaultRegion = options.region
+        ? options.region
+        : await choseDefaultRegions();
+    console.log("Deploying to region:", chalk.green(info.defaultRegion));
+    // set default org if passed in
+    // TODO: Prompt them with a list or orgs
+    info.organization = (_a = options.org) !== null && _a !== void 0 ? _a : "personal";
+    console.log("Deploying to organization:", chalk.green(info.organization));
+    // turn our info object into default fly args
+    const defaultArgs = getDefaultFlyArgs(info);
+    if (!options.dbUrl) {
+        // deploy database
+        await deployDatabase(defaultArgs);
+        console.log(chalk.green("You successfully deployed your database!"));
+    }
+    const spinner = ora({
+        text: `Checking if database was deployed correctly...`,
+        color: "yellow",
+    }).start();
+    spinner.stop();
+    // deploy api
+    await deployMeta(defaultArgs);
+}
+// ---------------------------------------------
+// You are enterning function forest
+// Proceed with caution
+// ---------------------------------------------
+// Create default cli args like org and region to make life easier
+function getDefaultFlyArgs(args) {
+    let argsArray = ["--force-machines", "--auto-confirm"];
+    if (args.organization) {
+        argsArray.push("--org", args.organization);
+    }
+    if (args.defaultRegion) {
+        argsArray.push("--region", args.defaultRegion);
+    }
+    return argsArray;
+}
+async function userAuth(options, spinner) {
     let username = await whoami();
     if (!username) {
         console.log("Auhtoritizing with Fly.io");
@@ -31,6 +88,7 @@ async function main() {
         username = await whoami();
     }
     else if (!options.yes) {
+        spinner.stop();
         const resp = await confirm({
             message: `You are logged into Fly.io as: ${username}. Do you want to continue?`,
             default: true,
@@ -40,25 +98,105 @@ async function main() {
             username = await whoami();
         }
     }
-    console.log("Deploying :", chalk.green(username));
+    return username;
 }
 async function flyLogin() {
-    var _a, e_1, _b, _c;
     const flyLoginSpawn = spawn("fly", ["auth", "login"]);
-    flyLoginSpawn.on("error", (err) => {
+    return await execAsync(flyLoginSpawn);
+}
+async function whoami() {
+    const whoamiSpawn = spawn("fly", ["auth", "whoami"]);
+    return await execAsync(whoamiSpawn);
+}
+async function choseDefaultRegions() {
+    let options = [
+        {
+            city: "",
+            code: "",
+        },
+    ];
+    const regionsSpawn = spawn("fly", ["platform", "regions"]);
+    const regions = await execAsync(regionsSpawn);
+    const regionChoices = regions.split("\n").slice(1);
+    for (let i = 0; i < regionChoices.length; i++) {
+        const infoArray = regionChoices[i].split(`\t`);
+        if (infoArray[1] && infoArray[0]) {
+            options.push({
+                city: infoArray[1],
+                code: infoArray[0].trim(),
+            });
+        }
+    }
+    options = options.filter((o) => o.city !== "");
+    return await select({
+        message: "Select a default region",
+        choices: options.map((o) => {
+            return {
+                name: o.city + " " + o.code,
+                value: o.code,
+            };
+        }),
+    });
+}
+async function deployMeta(userDefaultArgs) {
+    const metaName = await input({
+        message: "Enter a name for your postgres metadata instance, or leave blank for a generated one",
+    });
+    // if we dont have a name passed in, we need to generate one
+    const nameCommands = metaName ? ["--name", metaName] : ["--generate-name"];
+    // create array of commands
+    const metalaunchCommandArray = ["launch"].concat(launchDefaultArgs, userDefaultArgs, nameCommands);
+    // run fly launch --no-deploy to allocate app
+    const metaLaunch = spawn("fly", metalaunchCommandArray, {
+        cwd: "../pg-meta",
+    });
+    await execAsyncLog(metaLaunch);
+    await allocatePrivateV6("../pg-meta");
+    await flyDeploy("../pg-meta");
+    console.log(chalk.blue("Deploying metadata"));
+    return;
+}
+async function deployDatabase(userDefaultArgs) {
+    const dbName = await input({
+        message: "Enter a name for your database, or leave blank for a generated one",
+    });
+    // if we dont have a name passed in, we need to generate one
+    const nameCommands = dbName ? ["--name", dbName] : ["--generate-name"];
+    // create array of commands
+    const launchCommandArray = ["launch", "--internal-port", "5432"].concat(launchDefaultArgs, userDefaultArgs, nameCommands);
+    // run fly launch --no-deploy to allocate app
+    const dbLaunch = spawn("fly", launchCommandArray, {
+        cwd: "../../packages/database",
+    });
+    const resp = await execAsync(dbLaunch);
+    console.log(resp);
+    await allocatePrivateV6("../../packages/database");
+    // run fly deploy --remote-only to deploy db
+    console.log(chalk.blue("Deploying database"));
+    const deployResp = await flyDeploy("../../packages/database");
+    const machine = spawn("fly", ["scale", "memory", "1024"], {
+        cwd: "../../packages/database",
+    });
+    await execAsyncLog(machine);
+    console.log("db deploy: \n", deployResp);
+    return;
+}
+async function execAsync(spawn) {
+    var _a, e_1, _b, _c;
+    let response = "";
+    spawn.on("error", (err) => {
         console.log(`error: ${err.message}`);
     });
-    flyLoginSpawn.stderr.on("error", (data) => {
+    spawn.stderr.on("error", (data) => {
         console.log(`stderr: ${data}`);
     });
-    let loginData = "";
     try {
-        for (var _d = true, _e = __asyncValues(flyLoginSpawn.stdout), _f; _f = await _e.next(), _a = _f.done, !_a;) {
+        for (var _d = true, _e = __asyncValues(spawn.stdout), _f; _f = await _e.next(), _a = _f.done, !_a;) {
             _c = _f.value;
             _d = false;
             try {
                 const data = _c;
-                loginData = data.toString().trim();
+                response += data.toString();
             }
             finally {
                 _d = true;
@@ -72,25 +210,25 @@ async function flyLogin() {
         }
         finally { if (e_1) throw e_1.error; }
     }
-    return loginData;
+    return response;
 }
-async function whoami() {
+async function execAsyncLog(spawn) {
     var _a, e_2, _b, _c;
-    const whoamiSpawn = spawn("fly", ["auth", "whoami"]);
-    whoamiSpawn.on("error", (err) => {
+    let response = "";
+    spawn.on("error", (err) => {
         console.log(`error: ${err.message}`);
     });
-    whoamiSpawn.stderr.on("error", (data) => {
+    spawn.stderr.on("error", (data) => {
         console.log(`stderr: ${data}`);
     });
-    let username = "";
     try {
-        for (var _d = true, _e = __asyncValues(whoamiSpawn.stdout), _f; _f = await _e.next(), _a = _f.done, !_a;) {
+        for (var _d = true, _e = __asyncValues(spawn.stdout), _f; _f = await _e.next(), _a = _f.done, !_a;) {
             _c = _f.value;
             _d = false;
             try {
                 const data = _c;
-                username = data.toString().trim();
+                console.log(data.toString());
+                response += data.toString();
             }
             finally {
                 _d = true;
@@ -104,6 +242,24 @@ async function whoami() {
         }
         finally { if (e_2) throw e_2.error; }
     }
-    return username;
+    return response;
 }
+async function allocatePrivateV6(path) {
+    const ips = spawn("fly", ["ips", "allocate-v6", "--private"], {
+        cwd: path,
+    });
+    return await execAsyncLog(ips);
+}
+async function flyDeploy(path) {
+    const flyDeploy = spawn("fly", ["deploy"], {
+        cwd: path,
+    });
+    return await execAsyncLog(flyDeploy);
+}
+const launchDefaultArgs = [
+    "--no-deploy",
+    "--copy-config",
+    "--reuse-app",
+    "--force-machines",
+];
 //# sourceMappingURL=index.mjs.map
--- apps/cli/src/index.mts
+++ apps/cli/src/index.mts
@@ -1,26 +1,89 @@
-import { spawn, exec } from "child_process";
+import { spawn, exec, ChildProcessWithoutNullStreams } from "child_process";
 import figlet from "figlet";
-import { Command } from "commander";
-import { select, confirm } from "@inquirer/prompts";
+import { Command, OptionValues } from "commander";
+import { select, confirm, input } from "@inquirer/prompts";
 import chalk from "chalk";
+import ora, { Ora } from "ora";
+console.log(figlet.textSync("Supa", "Larry 3D"));
+console.log(figlet.textSync("Based", "Larry 3D"));
 const program = new Command();
+program
+  .version("0.0.1")
+  .description("Deploy Supbase to Fly.io")
+  .option("-O, --org  [value]", "Fly.io Target Organization")
+  .option("-y, --yes", "Skip prompts and deploy")
+  .option("-r, --region [value]", "Fly.io Target Region")
+  .option("--dbUrl [value]", "Fly.io Target Region")
+  .parse(process.argv);
+const options = program.opts();
 
-// Async Iteration available since Node 10
-main();
+takeoff();
 
-async function main() {
-  program
-    .version("0.0.1")
-    .description("Deploy Supbase to Fly.io")
-    .option("-o, --org  [value]", "Fly.io Target Organization")
-    .option("-y, --yes", "Skip prompts and deploy")
-    .parse(process.argv);
+async function takeoff() {
+  // create info object to pass around
+  let info: cliInfo = {
+    username: "",
+    defaultRegion: "",
+    organization: "",
+  };
 
-  const options = program.opts();
+  const authSpinner = ora({
+    text: `Checking fly cli authorization...`,
+    color: "yellow",
+  }).start();
 
-  console.log(figlet.textSync("Supa", "Larry 3D"));
-  console.log(figlet.textSync("Based", "Larry 3D"));
+  // grab username
+  info.username = await userAuth(options, authSpinner);
+  authSpinner.stop();
+  console.log("Deploying database as:", chalk.green(info.username));
 
+  // chose default region if not passed in
+  info.defaultRegion = options.region
+    ? options.region
+    : await choseDefaultRegions();
+  console.log("Deploying to region:", chalk.green(info.defaultRegion));
+
+  // set default org if passed in
+  // TODO: Prompt them with a list or orgs
+
+  info.organization = options.org ?? "personal";
+  console.log("Deploying to organization:", chalk.green(info.organization));
+
+  // turn our info object into default fly args
+  const defaultArgs = getDefaultFlyArgs(info);
+  if (!options.dbUrl) {
+    // deploy database
+    await deployDatabase(defaultArgs);
+    console.log(chalk.green("You successfully deployed your database!"));
+  }
+  const spinner = ora({
+    text: `Checking if database was deployed correctly...`,
+    color: "yellow",
+  }).start();
+
+  spinner.stop();
+
+  // deploy api
+  await deployMeta(defaultArgs);
+}
+
+// ---------------------------------------------
+// You are enterning function forest
+// Proceed with caution
+// ---------------------------------------------
+// Create default cli args like org and region to make life easier
+function getDefaultFlyArgs(args: cliInfo) {
+  let argsArray = ["--force-machines", "--auto-confirm"];
+  if (args.organization) {
+    argsArray.push("--org", args.organization);
+  }
+  if (args.defaultRegion) {
+    argsArray.push("--region", args.defaultRegion);
+  }
+  return argsArray;
+}
+
+async function userAuth(options: OptionValues, spinner: Ora) {
   let username = await whoami();
   if (!username) {
     console.log("Auhtoritizing with Fly.io");
@@ -28,6 +91,7 @@ async function main() {
     await flyLogin();
     username = await whoami();
   } else if (!options.yes) {
+    spinner.stop();
     const resp = await confirm({
       message: `You are logged into Fly.io as: ${username}. Do you want to continue?`,
       default: true,
@@ -37,40 +101,170 @@ async function main() {
       username = await whoami();
     }
   }
-
-  console.log("Deploying :", chalk.green(username));
+  return username;
 }
 
 async function flyLogin() {
   const flyLoginSpawn = spawn("fly", ["auth", "login"]);
-  flyLoginSpawn.on("error", (err) => {
-    console.log(`error: ${err.message}`);
-  });
-
-  flyLoginSpawn.stderr.on("error", (data) => {
-    console.log(`stderr: ${data}`);
-  });
-  let loginData = "";
-
-  for await (const data of flyLoginSpawn.stdout) {
-    loginData = data.toString().trim();
-  }
-  return loginData;
+  return await execAsync(flyLoginSpawn);
 }
 
 async function whoami() {
   const whoamiSpawn = spawn("fly", ["auth", "whoami"]);
-  whoamiSpawn.on("error", (err) => {
+  return await execAsync(whoamiSpawn);
+}
+
+async function choseDefaultRegions() {
+  let options = [
+    {
+      city: "",
+      code: "",
+    },
+  ];
+  const regionsSpawn = spawn("fly", ["platform", "regions"]);
+  const regions = await execAsync(regionsSpawn);
+  const regionChoices = regions.split("\n").slice(1);
+  for (let i = 0; i < regionChoices.length; i++) {
+    const infoArray = regionChoices[i].split(`\t`);
+    if (infoArray[1] && infoArray[0]) {
+      options.push({
+        city: infoArray[1],
+        code: infoArray[0].trim(),
+      });
+    }
+  }
+  options = options.filter((o) => o.city !== "");
+  return await select({
+    message: "Select a default region",
+    choices: options.map((o) => {
+      return {
+        name: o.city + " " + o.code,
+        value: o.code,
+      };
+    }),
+  });
+}
+
+async function deployMeta(userDefaultArgs: string[]) {
+  const metaName = await input({
+    message:
+      "Enter a name for your postgres metadata instance, or leave blank for a generated one",
+  });
+
+  // if we dont have a name passed in, we need to generate one
+  const nameCommands = metaName ? ["--name", metaName] : ["--generate-name"];
+
+  // create array of commands
+  const metalaunchCommandArray = ["launch"].concat(
+    launchDefaultArgs,
+    userDefaultArgs,
+    nameCommands
+  );
+
+  // run fly launch --no-deploy to allocate app
+  const metaLaunch = spawn("fly", metalaunchCommandArray, {
+    cwd: "../pg-meta",
+  });
+  await execAsyncLog(metaLaunch);
+  await allocatePrivateV6("../pg-meta");
+  await flyDeploy("../pg-meta");
+  console.log(chalk.blue("Deploying metadata"));
+  return;
+}
+
+async function deployDatabase(userDefaultArgs: string[]) {
+  const dbName = await input({
+    message:
+      "Enter a name for your database, or leave blank for a generated one",
+  });
+
+  // if we dont have a name passed in, we need to generate one
+  const nameCommands = dbName ? ["--name", dbName] : ["--generate-name"];
+
+  // create array of commands
+  const launchCommandArray = ["launch", "--internal-port", "5432"].concat(
+    launchDefaultArgs,
+    userDefaultArgs,
+    nameCommands
+  );
+
+  // run fly launch --no-deploy to allocate app
+  const dbLaunch = spawn("fly", launchCommandArray, {
+    cwd: "../../packages/database",
+  });
+  const resp = await execAsync(dbLaunch);
+  console.log(resp);
+
+  await allocatePrivateV6("../../packages/database");
+
+  // run fly deploy --remote-only to deploy db
+  console.log(chalk.blue("Deploying database"));
+  const deployResp = await flyDeploy("../../packages/database");
+  const machine = spawn("fly", ["scale", "memory", "1024"], {
+    cwd: "../../packages/database",
+  });
+
+  await execAsyncLog(machine);
+  console.log("db deploy: \n", deployResp);
+  return;
+}
+
+async function execAsync(spawn: ChildProcessWithoutNullStreams) {
+  let response = "";
+  spawn.on("error", (err) => {
     console.log(`error: ${err.message}`);
   });
 
-  whoamiSpawn.stderr.on("error", (data) => {
+  spawn.stderr.on("error", (data) => {
     console.log(`stderr: ${data}`);
   });
-  let username = "";
 
-  for await (const data of whoamiSpawn.stdout) {
-    username = data.toString().trim();
+  for await (const data of spawn.stdout) {
+    response += data.toString();
   }
-  return username;
+  return response;
 }
+async function execAsyncLog(spawn: ChildProcessWithoutNullStreams) {
+  let response = "";
+  spawn.on("error", (err) => {
+    console.log(`error: ${err.message}`);
+  });
+
+  spawn.stderr.on("error", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  for await (const data of spawn.stdout) {
+    console.log(data.toString());
+    response += data.toString();
+  }
+  return response;
+}
+
+async function allocatePrivateV6(path: string) {
+  const ips = spawn("fly", ["ips", "allocate-v6", "--private"], {
+    cwd: path,
+  });
+
+  return await execAsyncLog(ips);
+}
+
+async function flyDeploy(path: string) {
+  const flyDeploy = spawn("fly", ["deploy"], {
+    cwd: path,
+  });
+  return await execAsyncLog(flyDeploy);
+}
+
+const launchDefaultArgs = [
+  "--no-deploy",
+  "--copy-config",
+  "--reuse-app",
+  "--force-machines",
+];
+
+type cliInfo = {
+  username: string;
+  defaultRegion: string;
+  organization: string;
+};
